01b56aa0b963f581c5136584c38f5a0db8cbb66b
emkab <me@emkab.dev>
2025-02-01 23:28:48 +0200

    Unfinished parser implementation.

--- src/parser.ts
+++ src/parser.ts
@@ -1,9 +1,40 @@
+import { HDate } from "@hebcal/core";
 import { Lexer } from "./lexer";
+import { Token } from "./types";
 
 export default class Parser {
-	constructor(private lexer: Lexer) {}
+	private lexer: Lexer;
+	private source: string;
+	private tokens: Token[];
 
-	parse() {
-		// ...TBI
+	private date: HDate | undefined;
+
+	constructor(source: string, date?: Date | HDate | undefined) {
+		this.lexer = new Lexer(source);
+		this.source = source;
+		this.tokens = this.lexer.tokenize();
+
+		if (!date) {
+			this.date = new HDate();
+		} else if (date instanceof Date) {
+			this.date = new HDate(date);
+		} else if (date instanceof HDate) {
+			this.date = date;
+		}
 	}
+
+	parse() {}
+
+	calculateKeywords() {
+		this.parsedKeywords["DAY"] = this.date!.getDay().toString();
+	}
+
+	private parsedKeywords: { [key: string]: string } = {
+		DAY: "DDDD",
+		MONTH: "MMMM",
+		YEAR: "YYYY",
+		DAY_OF_WEEK: "ddd",
+		MONTH_NAME: "mmm",
+		YEAR_HEBREW: "yyyy",
+	};
 }
